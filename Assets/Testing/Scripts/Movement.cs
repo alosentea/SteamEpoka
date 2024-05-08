21cf7a5e29f7f67cafa0a4c7ef5db1f1a521d573
Alejandro <166367196+alosentea@users.noreply.github.com>
2024-05-08 10:23:29 +0200

    Proprietary movement script development
    [fv 0.3]
    + Jump by raycast added.

--- Assets/Testing/Scripts/Movement.cs
+++ Assets/Testing/Scripts/Movement.cs
@@ -9,9 +9,11 @@ public class Movement : MonoBehaviour
 
     public float walkThrust;
     float walkForce;
-    public float jumpForce;
-    
     public float maxVelocity;
+
+    public float jumpForce;
+    public float raycastDistance;
+    public LayerMask rayLayer;
 
     void Start()
     {
@@ -20,8 +22,12 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(PlayerRigidbody2D.velocity.x);
+        //Debug.Log(PlayerRigidbody2D.velocity.x);
         walkForce = walkThrust * Time.deltaTime;
+
+        RaycastHit2D JumpRay = Physics2D.Raycast(transform.position, -transform.up, raycastDistance, rayLayer);
+        Debug.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y-raycastDistance), Color.red);
+        Debug.Log(JumpRay.collider);
 
         void ForceMovement()
         {
@@ -39,11 +45,14 @@ public class Movement : MonoBehaviour
                 PlayerRigidbody2D.AddForce(transform.right * walkForce, ForceMode2D.Force);
             }
 
-            //if (PlayerIsNotInTheAir)
+            if (Input.GetKey(KeyCode.Space))
             {
-                if (Input.GetKey(KeyCode.Space))
+                if (JumpRay.collider != null)
                 {
-                    PlayerRigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Force);
+                    if (PlayerRigidbody2D.velocity.y <= 0)
+                    {
+                        PlayerRigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Force);
+                    }
                 }
             }
         }
